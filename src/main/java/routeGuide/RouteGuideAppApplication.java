90fde8237c13427957a61fc7edd6dc9a3c4520da
RameshBalireddi <ramesh.balireddi@imaginnovate.com>
2023-08-29 14:31:39 +0530

    cors

--- src/main/java/routeGuide/RouteGuideAppApplication.java
+++ src/main/java/routeGuide/RouteGuideAppApplication.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 //@ComponentScan("routeGuide")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:4200/*")
+
 public class RouteGuideAppApplication {
 
 	public static void main(String[] args) {
