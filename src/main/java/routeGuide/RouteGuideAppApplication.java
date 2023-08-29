1785df03a84ccb9fffd742e6413c7f85cab3362d
RameshBalireddi <ramesh.balireddi@imaginnovate.com>
2023-08-29 15:05:06 +0530

    cors

--- src/main/java/routeGuide/RouteGuideAppApplication.java
+++ src/main/java/routeGuide/RouteGuideAppApplication.java
@@ -2,8 +2,11 @@ package routeGuide;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @CrossOrigin(origins = "*")
 @SpringBootApplication
@@ -12,6 +15,16 @@ public class RouteGuideAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RouteGuideAppApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:4200");
+			}
+		};
 	}
 
 }
